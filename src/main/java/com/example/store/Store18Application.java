279fde4ccffffce5544942a96615d5e71dd38fb0
shamsiddin18 <shamsiddin042@mail.ru>
2022-08-26 18:49:57 +0500

    java

---
+++ src/main/java/com/example/store/Store18Application.java
@@ -0,0 +1,13 @@
+package com.example.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Store18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Store18Application.class, args);
+	}
+
+}
